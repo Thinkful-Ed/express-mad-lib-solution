775941b6b78c454f0090e28527dbe564a21f48e5
Benjamin E White <benjamin.e.white1@gmail.com>
2016-10-01 00:47:21 -0400

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// const doMadLib = (libTemplate, ani) => {};
-
 const doMadlib = (params) => {
   const {adjective1, adjective2, adjective3, adverb, name, pronoun, noun, place} = params;
   return (
@@ -22,9 +20,7 @@ const doMadlib = (params) => {
 };
 
 // GET requests to the root of the server
-app.get('/', (req, res) => {
-  res.send(doMadlib(req.query)).end()
-});
+app.get('/', (req, res) => res.send(doMadlib(req.query)).end());
 
 
 // listen for requests :)
