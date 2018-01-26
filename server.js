5affc8c613439c41afe9fac9e6748a8b6d754f98
Chris Tsongas <chris.tsongas@gmail.com>
2018-01-26 11:48:53 -0700

    Format response to match curriculum.
    It's the web...shouldn't we send <br> tags instead of \n so the output appears in the browser as intended?

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const doMadlib = (params) => {
   // then we return a string that substitutes in these values
   return (
   `There's a ${adjective1} new ${name} in ${place} and everyone's ` +
-  `talking. Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it.` + 
-  `However, ${name} has a secret - ${name}'s a vile vampire. \n` + 
+  `talking. Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it. ` + 
+  `However, ${name} has a secret - ${name}'s a vile vampire.<br><br>` + 
   `Will it end with a bite, or with a stake through the ${noun}?`);
 };
 
